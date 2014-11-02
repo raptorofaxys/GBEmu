b8c936af4513b79bfce43bd640854fb7040c35f2
fhamel <fhamel@gmail.com>
2014-11-02 12:57:05 -0500

    Add utilities, basic bootup sequence.
    add: Utils.h, Exception, Janitor
    
    change: emulator now loads Tetris ROM and sets SDL window title appropriately

--- GBEmuNative/Emulator.cpp
+++ GBEmuNative/Emulator.cpp
@@ -1,166 +1,241 @@
-/*
-  Copyright (C) 1997-2014 Sam Lantinga <slouken@libsdl.org>
-
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely.
-*/
-/********************************************************************************
- *                                                                              *
- * Running moose :) Coded by Mike Gorchak.                                      *
- *                                                                              *
- ********************************************************************************/
-
-#include <stdlib.h>
-#include <stdio.h>
+#include "Utils.h"
 
 #include "SDL.h"
 
-#define MOOSEPIC_W 64
-#define MOOSEPIC_H 88
+#include <stdlib.h>
+#include <stdio.h>
+#include <memory>
+#include <vector>
+#include <functional>
 
-#define MOOSEFRAME_SIZE (MOOSEPIC_W * MOOSEPIC_H)
-#define MOOSEFRAMES_COUNT 10
+#include <Windows.h>
 
-SDL_Color MooseColors[84] = {
-    {49, 49, 49, 255}, {66, 24, 0, 255}, {66, 33, 0, 255}, {66, 66, 66, 255},
-    {66, 115, 49, 255}, {74, 33, 0, 255}, {74, 41, 16, 255}, {82, 33, 8, 255},
-    {82, 41, 8, 255}, {82, 49, 16, 255}, {82, 82, 82, 255}, {90, 41, 8, 255},
-    {90, 41, 16, 255}, {90, 57, 24, 255}, {99, 49, 16, 255}, {99, 66, 24, 255},
-    {99, 66, 33, 255}, {99, 74, 33, 255}, {107, 57, 24, 255}, {107, 82, 41, 255},
-    {115, 57, 33, 255}, {115, 66, 33, 255}, {115, 66, 41, 255}, {115, 74, 0, 255},
-    {115, 90, 49, 255}, {115, 115, 115, 255}, {123, 82, 0, 255}, {123, 99, 57, 255},
-    {132, 66, 41, 255}, {132, 74, 41, 255}, {132, 90, 8, 255}, {132, 99, 33, 255},
-    {132, 99, 66, 255}, {132, 107, 66, 255}, {140, 74, 49, 255}, {140, 99, 16, 255},
-    {140, 107, 74, 255}, {140, 115, 74, 255}, {148, 107, 24, 255}, {148, 115, 82, 255},
-    {148, 123, 74, 255}, {148, 123, 90, 255}, {156, 115, 33, 255}, {156, 115, 90, 255},
-    {156, 123, 82, 255}, {156, 132, 82, 255}, {156, 132, 99, 255}, {156, 156, 156, 255},
-    {165, 123, 49, 255}, {165, 123, 90, 255}, {165, 132, 82, 255}, {165, 132, 90, 255},
-    {165, 132, 99, 255}, {165, 140, 90, 255}, {173, 132, 57, 255}, {173, 132, 99, 255},
-    {173, 140, 107, 255}, {173, 140, 115, 255}, {173, 148, 99, 255}, {173, 173, 173, 255},
-    {181, 140, 74, 255}, {181, 148, 115, 255}, {181, 148, 123, 255}, {181, 156, 107, 255},
-    {189, 148, 123, 255}, {189, 156, 82, 255}, {189, 156, 123, 255}, {189, 156, 132, 255},
-    {189, 189, 189, 255}, {198, 156, 123, 255}, {198, 165, 132, 255}, {206, 165, 99, 255},
-    {206, 165, 132, 255}, {206, 173, 140, 255}, {206, 206, 206, 255}, {214, 173, 115, 255},
-    {214, 173, 140, 255}, {222, 181, 148, 255}, {222, 189, 132, 255}, {222, 189, 156, 255},
-    {222, 222, 222, 255}, {231, 198, 165, 255}, {231, 231, 231, 255}, {239, 206, 173, 255}
+class Exception
+{
+public:
+	Exception(const char* pFormatter, ...)
+	{
+		va_list args;
+		va_start(args, pFormatter);
+		char szBuffer[4096];
+		vsnprintf_s(szBuffer, ARRAY_SIZE(szBuffer), pFormatter, args);
+		va_end(args);
+
+		m_error = szBuffer;
+	}
+
+	const char* GetMessage() const
+	{
+		return m_error.c_str();
+	}
+		
+private:
+	std::string m_error;
 };
 
-Uint8 MooseFrames[MOOSEFRAMES_COUNT][MOOSEFRAME_SIZE];
-
-void quit(int rc)
+void LoadFileAsByteArray(std::vector<Uint8>& output, const char* pFileName)
 {
-    SDL_Quit();
-    exit(rc);
+    SDL_RWops* pHandle = SDL_RWFromFile(pFileName, "rb");
+    
+	if (!pHandle)
+	{
+        throw Exception("Failed to load file %s.", pFileName);
+    }
+
+	SDL_RWseek(pHandle, 0,  RW_SEEK_END);
+	int fileSize = static_cast<int>(SDL_RWtell(pHandle));
+	SDL_RWseek(pHandle, 0,  RW_SEEK_SET);
+
+	output.resize(fileSize);
+	SDL_RWread(pHandle, output.data(), fileSize, 1);
+    SDL_RWclose(pHandle);
 }
 
-void UpdateTexture(SDL_Texture *texture, int frame)
+std::shared_ptr<std::vector<Uint8>> LoadFileAsByteArray(const char* pFileName)
 {
-    SDL_Color *color;
-    Uint8 *src;
-    Uint32 *dst;
-    int row, col;
-    void *pixels;
-    int pitch;
+	std::shared_ptr<std::vector<Uint8>> pData(new std::vector<Uint8>);
+	LoadFileAsByteArray(*pData, pFileName);
 
-    if (SDL_LockTexture(texture, NULL, &pixels, &pitch) < 0) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't lock texture: %s\n", SDL_GetError());
-        quit(5);
-    }
-    src = MooseFrames[frame];
-    for (row = 0; row < MOOSEPIC_H; ++row) {
-        dst = (Uint32*)((Uint8*)pixels + row * pitch);
-        for (col = 0; col < MOOSEPIC_W; ++col) {
-            color = &MooseColors[*src++];
-            *dst++ = (0xFF000000|(color->r<<16)|(color->g<<8)|color->b);
-        }
-    }
-    SDL_UnlockTexture(texture);
+	return pData;
 }
+
+class Rom
+{
+public:
+	Rom(const char* pFileName)
+	{
+		LoadFromFile(pFileName);
+	}
+
+	std::string GetRomName()
+	{
+		std::string result;
+
+		Uint8* p = &m_pRom[kNameOffset];
+		for (int i = 0; i < kNameLength; ++i)
+		{
+			if (*p)
+			{
+				result += *p;
+			}
+			++p;
+		}
+
+		return result;
+	}
+
+private:
+	static const int kNameOffset = 0x134;
+	static const int kNameLength = 0x11;
+
+	void LoadFromFile(const char* pFileName)
+	{
+		LoadFileAsByteArray(m_pRom, pFileName);
+	}
+
+	std::vector<Uint8> m_pRom;
+};
+
+class GameBoy
+{
+public:
+	static int const kScreenWidth = 160;
+	static int const kScreenHeight = 144;
+};
+
+//template<typename L>
+//struct Janitor
+//{
+//
+//};
+//
+//template<typename L> Janitor<L> MakeJanitor(L& lambda)
+//{
+//	return Janitor<L>(lambda);
+//}
+
+struct Janitor
+{
+	typedef std::function<void()> FuncType;
+	Janitor(FuncType f)
+		: m_func(f)
+		, m_released(false)
+	{
+	}
+
+	~Janitor()
+	{
+		if (!m_released)
+		{
+			Invoke();
+		}
+	}
+
+	void Release()
+	{
+		m_released = true;
+	}
+
+	void Invoke()
+	{
+		m_func();
+		Release();
+	}
+
+	bool m_released;
+	FuncType m_func;
+};
+
+class ProcessConsole
+{
+public:
+	ProcessConsole()
+	{
+		AllocConsole();
+		FILE* pFile;
+		freopen_s(&pFile, "CON", "w", stdout);
+	}
+
+	~ProcessConsole()
+	{
+		FreeConsole();
+	}
+};
 
 int main(int argc, char **argv)
-//int main()
 {
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_RWops *handle;
-    SDL_Texture *MooseTexture;
-    SDL_Event event;
-    SDL_bool done = SDL_FALSE;
-    int frame;
+	try
+	{
+		ProcessConsole console;
 
-	/* Enable standard application logging */
-    SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO);
+		SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO);
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s\n", SDL_GetError());
-        return 1;
-    }
+		if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		{
+			throw Exception("Couldn't initialize SDL: %s", SDL_GetError());
+		}
 
-    /* load the moose images */
-    handle = SDL_RWFromFile("moose.dat", "rb");
-    if (handle == NULL) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Can't find the file moose.dat !\n");
-        quit(2);
-    }
-    SDL_RWread(handle, MooseFrames, MOOSEFRAME_SIZE, MOOSEFRAMES_COUNT);
-    SDL_RWclose(handle);
+		Janitor j([] { SDL_Quit(); });
 
+		Rom rom("Tetris (JUE) (V1.1) [!].gb");
+		const auto& gameName = rom.GetRomName();
 
-    /* Create the window and renderer */
-    window = SDL_CreateWindow("Happy Moose",
-                              SDL_WINDOWPOS_UNDEFINED,
-                              SDL_WINDOWPOS_UNDEFINED,
-                              MOOSEPIC_W*4, MOOSEPIC_H*4,
-                              SDL_WINDOW_RESIZABLE);
-    if (!window) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't set create window: %s\n", SDL_GetError());
-        quit(3);
-    }
+		std::shared_ptr<SDL_Window> pWindow(SDL_CreateWindow(gameName.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, GameBoy::kScreenWidth * 4, GameBoy::kScreenHeight * 4, 0), SDL_DestroyWindow);
 
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    if (!renderer) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't set create renderer: %s\n", SDL_GetError());
-        quit(4);
-    }
+		if (!pWindow)
+		{
+			throw Exception("Couldn't create window");
+		}
+		
+		std::shared_ptr<SDL_Renderer> pRenderer(SDL_CreateRenderer(pWindow.get(), -1, 0), SDL_DestroyRenderer);
+		if (!pRenderer)
+		{
+			throw Exception("Couldn't create renderer");
+		}
 
-    MooseTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, MOOSEPIC_W, MOOSEPIC_H);
-    if (!MooseTexture) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't set create texture: %s\n", SDL_GetError());
-        quit(5);
-    }
+		std::shared_ptr<SDL_Texture> pFrameBuffer(SDL_CreateTexture(pRenderer.get(), SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, GameBoy::kScreenWidth, GameBoy::kScreenHeight), SDL_DestroyTexture);
+		if (!pRenderer)
+		{
+			throw Exception("Couldn't create framebuffer texture");
+		}
 
-    /* Loop, waiting for QUIT or the escape key */
-    frame = 0;
-    while (!done) {
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-            case SDL_KEYDOWN:
-                if (event.key.keysym.sym == SDLK_ESCAPE) {
-                    done = SDL_TRUE;
-                }
-                break;
-            case SDL_QUIT:
-                done = SDL_TRUE;
-                break;
-            }
-        }
+		SDL_Event event;
+		bool done = false;
 
-        frame = (frame + 1) % MOOSEFRAMES_COUNT;
-        UpdateTexture(MooseTexture, frame);
+		while (!done)
+		{
+		    while (SDL_PollEvent(&event))
+			{
+		        switch (event.type)
+				{
+		        case SDL_KEYDOWN:
+		            if (event.key.keysym.sym == SDLK_ESCAPE)
+					{
+		                done = true;
+		            }
+		            break;
+		        case SDL_QUIT:
+		            done = true;
+		            break;
+		        }
+		    }
 
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, MooseTexture, NULL, NULL);
-        SDL_RenderPresent(renderer);
-    }
-    SDL_DestroyRenderer(renderer);
+			void* pPixels;
+			int pitch;
+			SDL_LockTexture(pFrameBuffer.get(), NULL, &pPixels, &pitch);
+			Uint32* pARGB = static_cast<Uint32*>(pPixels);
+			*pARGB = rand();
+			SDL_UnlockTexture(pFrameBuffer.get());
 
-    quit(0);
+		    SDL_RenderClear(pRenderer.get());
+		    SDL_RenderCopy(pRenderer.get(), pFrameBuffer.get(), NULL, NULL);
+		    SDL_RenderPresent(pRenderer.get());
+		}
+	}
+	catch (const Exception& e)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Exception: %s", e.GetMessage());
+	}
+
     return 0;
 }
-
-/* vi: set ts=4 sw=4 expandtab: */
