7aae6434d193a83fa26d13e16515c5de6f080d84
fhamel <fhamel@gmail.com>
2014-11-02 21:02:06 -0500

    Emulator start - add Cpu/Memory classes.
    add: Cpu, Memory classes
    
    add: header to define memory-mapped registers; will probably change a bit
    add: basic execution flow with a handful of opcodes; will undoubtedly be refactored
    change: go back to a normal post-build step with the flaws that entails; the custom build step was very wonky and would cause infinite rebuild loops

--- GBEmuNative/Emulator.cpp
+++ GBEmuNative/Emulator.cpp
@@ -6,32 +6,8 @@
 #include <stdio.h>
 #include <memory>
 #include <vector>
-#include <functional>
 
 #include <Windows.h>
-
-class Exception
-{
-public:
-	Exception(const char* pFormatter, ...)
-	{
-		va_list args;
-		va_start(args, pFormatter);
-		char szBuffer[4096];
-		vsnprintf_s(szBuffer, ARRAY_SIZE(szBuffer), pFormatter, args);
-		va_end(args);
-
-		m_error = szBuffer;
-	}
-
-	const char* GetMessage() const
-	{
-		return m_error.c_str();
-	}
-		
-private:
-	std::string m_error;
-};
 
 void LoadFileAsByteArray(std::vector<Uint8>& output, const char* pFileName)
 {
@@ -67,11 +43,11 @@ public:
 		LoadFromFile(pFileName);
 	}
 
-	std::string GetRomName()
+	std::string GetRomName() const
 	{
 		std::string result;
 
-		Uint8* p = &m_pRom[kNameOffset];
+		const Uint8* p = &m_pRom[kNameOffset];
 		for (int i = 0; i < kNameLength; ++i)
 		{
 			if (*p)
@@ -82,6 +58,11 @@ public:
 		}
 
 		return result;
+	}
+
+	const std::vector<Uint8>& GetRom()
+	{
+		return m_pRom;
 	}
 
 private:
@@ -96,70 +77,291 @@ private:
 	std::vector<Uint8> m_pRom;
 };
 
-class GameBoy
+class Memory
 {
 public:
-	static int const kScreenWidth = 160;
-	static int const kScreenHeight = 144;
-};
+	static const int kRomBase = 0x0000;
+	static const int kRomBaseBank00Size = 0x4000;
+	
+	static const int kWorkMemoryBase = 0xC000;
+	static const int kWorkMemorySize = 0x2000; // 8k
 
-//template<typename L>
-//struct Janitor
-//{
-//
-//};
-//
-//template<typename L> Janitor<L> MakeJanitor(L& lambda)
-//{
-//	return Janitor<L>(lambda);
-//}
+	// Define memory addresses for all the memory-mapped registers
+	enum class MemoryMappedRegisters
+	{
+#define DEFINE_MEMORY_MAPPED_REGISTER_RW(addx, name) name = addx,
+#include "MemoryMappedRegisters.inc"
+#undef DEFINE_MEMORY_MAPPED_REGISTER_RW
+		//IF = 0xFF0F, // Interrupt Flag RW
+		//IE = 0xFFFF, // Interrupt Enable RW
+	};
 
-struct Janitor
-{
-	typedef std::function<void()> FuncType;
-	Janitor(FuncType f)
-		: m_func(f)
-		, m_released(false)
+	Memory(const std::shared_ptr<Rom>& rom)
+		: m_pRom(rom)
 	{
 	}
 
-	~Janitor()
+	Uint8 Read8(Uint16 address)
 	{
-		if (!m_released)
+		if (IsAddressInRange(address, kRomBase, kRomBaseBank00Size))
 		{
-			Invoke();
+			return m_pRom->GetRom()[address];
+		}
+		else if (IsAddressInRange(address, kWorkMemoryBase, kWorkMemorySize))
+		{
+			return m_workMemory[address - kWorkMemoryBase];
+		}
+		else
+		{
+			switch (address)
+			{
+				// Handle reads to all the memory-mapped registers
+#define DEFINE_MEMORY_MAPPED_REGISTER_RW(addx, name) case MemoryMappedRegisters::name: return m_##name;
+#include "MemoryMappedRegisters.inc"
+#undef DEFINE_MEMORY_MAPPED_REGISTER_RW
+			default:
+				throw NotImplementedException();
+			}
 		}
 	}
 
-	void Release()
+	//Uint16 Read16(Uint16 address)
+	//{
+	//	// Must handle as two reads, because the address can cross range boundaries
+	//	return Read8(address
+	//}
+
+	void Write8(Uint16 address, Uint8 value)
 	{
-		m_released = true;
+		if (IsAddressInRange(address, kRomBase, kRomBaseBank00Size))
+		{
+			throw Exception("Attempted to write to ROM area.");
+		}
+		else if (IsAddressInRange(address, kWorkMemoryBase, kWorkMemorySize))
+		{
+			m_workMemory[address - kWorkMemoryBase] = value;
+		}
+		else
+		{
+			switch (address)
+			{
+				// Handle writes to all the memory-mapped registers
+#define DEFINE_MEMORY_MAPPED_REGISTER_RW(addx, name) case MemoryMappedRegisters::name: m_##name = value; break;
+#include "MemoryMappedRegisters.inc"
+#undef DEFINE_MEMORY_MAPPED_REGISTER_RW
+			default:
+				throw NotImplementedException();
+			}
+		}
 	}
 
-	void Invoke()
-	{
-		m_func();
-		Release();
-	}
+private:
 
-	bool m_released;
-	FuncType m_func;
+	bool IsAddressInRange(Uint16 address, Uint16 base, Uint16 rangeSize)
+	{
+		return (address >= base) && (address < base + rangeSize);
+	}
+	
+	// Declare storage for all the memory-mapped registers
+#define DEFINE_MEMORY_MAPPED_REGISTER_RW(addx, name) Uint8 m_##name;
+#include "MemoryMappedRegisters.inc"
+#undef DEFINE_MEMORY_MAPPED_REGISTER_RW
+
+	std::shared_ptr<Rom> m_pRom;
+	char m_workMemory[kWorkMemorySize];
 };
 
-class ProcessConsole
+class Cpu
 {
 public:
-	ProcessConsole()
+	static Uint32 const kCyclesPerSecond = 4194304;
+
+	Cpu(const std::shared_ptr<Memory>& memory)
+		: m_pMemory(memory)
 	{
-		AllocConsole();
-		FILE* pFile;
-		freopen_s(&pFile, "CON", "w", stdout);
+		Reset();
 	}
 
-	~ProcessConsole()
+	void Reset()
 	{
-		FreeConsole();
+		m_cyclesRemaining = 0.0f;
+
+		IME = true;
+
+		static_assert(offsetof(Cpu, F) == offsetof(Cpu, AF), "Target machine is not little-endian; register unions must be revised");
+		PC = 0x0100;
+		AF = 0x01B0;
+		BC = 0x0013;
+		DE = 0x00D8;
+		HL = 0x014D;
+
+		//(*m_pMemory)[0xFF05] = 0x00;
 	}
+
+	void Update(float seconds)
+	{
+		m_cyclesRemaining += seconds * kCyclesPerSecond;
+		
+		while (m_cyclesRemaining > 0)
+		{
+			m_cyclesConsumedByCurrentInstruction = 0;
+			Uint8 opcode = Fetch8();
+
+			Sint32 instructionCycles = -1;
+
+			switch (opcode)
+			{
+				//@TODO: this will undoubtedly grow and be refactored.  Just want to have a few use cases to factor right.
+			case 0x0: // NOP
+				{
+					instructionCycles = 4;
+				}
+				break;
+			case 0x21: // LD HL,nn
+				{
+					instructionCycles = 12;
+					HL = Fetch16();
+				}
+				break;
+			case 0x31: // LD SP,nn
+				{
+					instructionCycles = 12;
+					SP = Fetch16();
+				}
+				break;
+			case 0x3E: // LD A,n
+				{
+					instructionCycles = 8;
+					A = Fetch8();
+				}
+				break;
+			case 0xC3: // JP nn
+				{
+					instructionCycles = 12;
+					auto target = Fetch16();
+					PC = target;
+				}
+				break;
+			case 0xCD: // CALL nn
+				{
+					throw NotImplementedException();
+				}
+				break;
+			case 0xE0: // LD (0xFF00+n),A
+				{
+					instructionCycles = 12;
+					auto disp = Fetch8();
+					auto address = disp + 0xFF00;
+					Write8(address, A);
+				}
+				break;
+			case 0xEA: // LD (nn),A
+				{
+					instructionCycles = 16;
+					auto address = Fetch16();
+					Write8(address, A);
+				}
+				break;
+			case 0xF3: // DI
+				{
+					instructionCycles = 4;
+					IME = false;
+				}
+				break;
+			default:
+				SDL_assert(false && "Unknown opcode encountered");
+			}
+
+			// Try to time things automatically, 
+			SDL_assert(instructionCycles != -1);
+			SDL_assert(m_cyclesConsumedByCurrentInstruction == instructionCycles);
+		}
+	}
+
+private:
+	void ConsumeCycles(Sint32 numCycles)
+	{
+		m_cyclesConsumedByCurrentInstruction += numCycles;
+	}
+
+	Uint8 Fetch8()
+	{
+		auto result = m_pMemory->Read8(PC);
+		++PC;
+		ConsumeCycles(4);
+		return result;
+	}
+
+	Uint16 Fetch16()
+	{
+		auto low = Fetch8();
+		auto high = Fetch8();
+		return Make16(high, low);
+	}
+
+	void Write8(Uint16 address, Uint8 value)
+	{
+		m_pMemory->Write8(address, value);
+		ConsumeCycles(4);
+	}
+
+	// This macro helps define register pairs that have alternate views.  For example, B and C can be indexed individually as 8-bit registers, but they can also be indexed together as a 16-bit register called BC. 
+	// A static_assert helps make sure the machine endianness behaves as expected.
+#define DualViewRegisterPair(High, Low) \
+	union \
+	{ \
+		struct \
+		{ \
+			Uint8 Low; \
+			Uint8 High; \
+		}; \
+		Uint16 High##Low; \
+	};
+
+    DualViewRegisterPair(A, F)
+    DualViewRegisterPair(B, C)
+    DualViewRegisterPair(D, E)
+    DualViewRegisterPair(H, L)
+
+#undef DualViewRegisterPair
+
+	Uint16 SP;
+	Uint16 PC;
+	bool IME; // whether interrupts are enabled - very special register, not memory-mapped
+
+	float m_cyclesRemaining;
+	Sint32 m_cyclesConsumedByCurrentInstruction;
+
+	std::shared_ptr<Memory> m_pMemory;
+};
+
+class GameBoy
+{
+public:
+	static const int kScreenWidth = 160;
+	static const int kScreenHeight = 144;
+
+	GameBoy(const char* pFileName)
+	{
+		m_pRom.reset(new Rom(pFileName));
+		m_pMemory.reset(new Memory(m_pRom));
+		m_pCpu.reset(new Cpu(m_pMemory));
+	}
+
+	const Rom& GetRom() const
+	{
+		return *m_pRom;
+	}
+
+	void Update(float seconds)
+	{
+		m_pCpu->Update(seconds);
+	}
+
+private:
+	std::shared_ptr<Rom> m_pRom;
+	std::shared_ptr<Memory> m_pMemory;
+	std::shared_ptr<Cpu> m_pCpu;
 };
 
 int main(int argc, char **argv)
@@ -177,8 +379,10 @@ int main(int argc, char **argv)
 
 		Janitor j([] { SDL_Quit(); });
 
-		Rom rom("Tetris (JUE) (V1.1) [!].gb");
-		const auto& gameName = rom.GetRomName();
+		//GameBoy gb("Tetris (JUE) (V1.1) [!].gb");
+		GameBoy gb("cpu_instrs.gb");
+		//Rom rom("Tetris (JUE) (V1.1) [!].gb");
+		const auto& gameName = gb.GetRom().GetRomName();
 
 		std::shared_ptr<SDL_Window> pWindow(SDL_CreateWindow(gameName.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, GameBoy::kScreenWidth * 4, GameBoy::kScreenHeight * 4, 0), SDL_DestroyWindow);
 
@@ -202,6 +406,7 @@ int main(int argc, char **argv)
 		SDL_Event event;
 		bool done = false;
 
+		Uint32 lastTicks = SDL_GetTicks();
 		while (!done)
 		{
 		    while (SDL_PollEvent(&event))
@@ -219,6 +424,10 @@ int main(int argc, char **argv)
 		            break;
 		        }
 		    }
+
+			Uint32 ticks = SDL_GetTicks();
+			gb.Update((ticks - lastTicks) / 1000.0f);
+			lastTicks = ticks;
 
 			void* pPixels;
 			int pitch;
--- GBEmuNative/Utils.h
+++ GBEmuNative/Utils.h
@@ -1,3 +1,104 @@
 #pragma once
 
+#include <functional>
+
+#include "SDL.h"
+#include <Windows.h>
+
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
+
+//template <typename T>
+//bool IsBetween(T value, T min, T max) { return (value >= min) && (value <= max); }
+//bool IsBetween(Uint16 value, Uint16 min, Uint16 max) { return IsBetween<Uint16>(value, min, max); }
+
+Uint16 Make16(Uint8 high, Uint8 low) { return (high << 8) | low; }
+
+class Exception
+{
+public:
+	Exception(const char* pFormatter, ...)
+	{
+		SDL_assert(false && "Exception being constructed");
+
+		va_list args;
+		va_start(args, pFormatter);
+		char szBuffer[4096];
+		vsnprintf_s(szBuffer, ARRAY_SIZE(szBuffer), pFormatter, args);
+		va_end(args);
+
+		m_error = szBuffer;
+	}
+
+	const char* GetMessage() const
+	{
+		return m_error.c_str();
+	}
+		
+private:
+	std::string m_error;
+};
+
+class NotImplementedException : public Exception
+{
+public:
+	NotImplementedException() : Exception("The function is not implemented.") {}
+};
+
+//template<typename L>
+//struct Janitor
+//{
+//
+//};
+//
+//template<typename L> Janitor<L> MakeJanitor(L& lambda)
+//{
+//	return Janitor<L>(lambda);
+//}
+
+struct Janitor
+{
+	typedef std::function<void()> FuncType;
+	Janitor(FuncType f)
+		: m_func(f)
+		, m_released(false)
+	{
+	}
+
+	~Janitor()
+	{
+		if (!m_released)
+		{
+			Invoke();
+		}
+	}
+
+	void Release()
+	{
+		m_released = true;
+	}
+
+	void Invoke()
+	{
+		m_func();
+		Release();
+	}
+
+	bool m_released;
+	FuncType m_func;
+};
+
+class ProcessConsole
+{
+public:
+	ProcessConsole()
+	{
+		AllocConsole();
+		FILE* pFile;
+		freopen_s(&pFile, "CON", "w", stdout);
+	}
+
+	~ProcessConsole()
+	{
+		FreeConsole();
+	}
+};
