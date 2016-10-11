7e8eab13424ec53c3691ca3c3c3427da4aedc8d5
fhamel <fhamel@gmail.com>
2016-10-11 09:51:19 -0400

    Flush tracelog on exit.

--- GBEmuNative/TraceLog.cpp
+++ GBEmuNative/TraceLog.cpp
@@ -4,4 +4,12 @@ namespace TraceLog
 {
 	std::string s_traceLog;
 	bool s_enabled;
+	struct FlushOnDestroy
+	{
+		~FlushOnDestroy()
+		{
+			TraceLog::Flush();
+		}
+	};
+	FlushOnDestroy s_flushOnDestroy;
 }
